6380edeef0f900f0850328a2dd18b7bf9ac9052d
prottoy5 <106944019+prottoy5@users.noreply.github.com>
2024-04-30 15:37:29 +0600

    Initial commit

---
+++ lib/models/gallery.dart
@@ -0,0 +1,10 @@
+class Gallery {
+  String img;
+  String title;
+  String longTitle;
+  String description;
+
+  Gallery({required this.img, required this.title,
+  required this.longTitle, required this.description});
+
+}
